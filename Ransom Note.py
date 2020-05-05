bedd8198f4315ca143c84f2dfcdcda273a2557dd
mananiac <manan.sagar@yahoo.com>
2020-05-05 07:03:44 +0530

    Ransom Original

--- Ransom Note.py
+++ Ransom Note.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         a,b=sorted(ransomNote),sorted(magazine)
@@ -10,17 +10,3 @@ class Solution:
             else:
                 return (False)
 
-=======
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        a,b=sorted(ransomNote),sorted(magazine)
-        for i in range(len(a)):
-        # for j in range(i,len(b)):
-            if a[i] in b[i:]:
-                y=b.index(a[i])
-                b[y]=-1
-            else:
-                return (False)
-
->>>>>>> e89d3c3c720b907e8c04b73b71c43de74857f12d
-        return (True)
