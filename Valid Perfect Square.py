aab841d5fd477288622fcd3abad545c19d529088
mananiac <manan.sagar@yahoo.com>
2020-05-10 11:07:51 +0530

    Day 9

---
+++ Valid Perfect Square.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num <2:
+            return True
+        l,r=1,num//2
+        #
+        while(l<=r):
+            mid = (l+r)//2
+            if (mid*mid) == num:
+                return True
+            elif (mid*mid)< num :
+                l=mid+1
+            elif (mid*mid)> num :
+                r=mid-1
+        return False
